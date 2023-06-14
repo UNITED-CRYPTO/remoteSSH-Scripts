3e8f6d6e94e3e1d4ab317960b64fdfc1b78ed7ab
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2023-06-14 12:03:55 +0500

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,12 @@
-touch test.txt 2> /dev/null
-if [ $? -eq 0 ]
-then
-  echo "Successfully created file"
-else
-  echo "Could not create file" >&2
-fi
-echo "Hello world!"
-set
-$HOME/.cargo/bin/bazuka init --mnemonic "$TEST"
+#touch test.txt 2> /dev/null
+#if [ $? -eq 0 ]
+#then
+#  echo "Successfully created file"
+#else
+#  echo "Could not create file" >&2
+#fi
+#echo "Hello world!"
+#set
+#$HOME/.cargo/bin/bazuka init --mnemonic "$TEST"
+echo "Print env from bash"
+printenv
