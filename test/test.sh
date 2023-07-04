0e84d0d0e5a0b10721802b18dddfdf375696374b
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2023-07-04 15:12:23 +0500

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -10,5 +10,5 @@
 #$HOME/.cargo/bin/bazuka init --mnemonic "$TEST"
 #echo "printenv from bash"
 #source ~/.bash_profile
-echo "printenv from bash" && printenv
+echo "printenv from bash" && printenv TEST
 bazuka node status | grep -e "version" -e "height" -e "network"
