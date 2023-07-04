8842118a00d639b6383834106fc172ab69dd396a
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2023-07-04 18:39:57 +0500

    Update ziesha_upd.sh

--- ziesha_upd.sh
+++ ziesha_upd.sh
@@ -1,11 +1,13 @@
-cd bazuka && git pull && cargo update && cargo install --path . && rm -rf ~/.bazuka ~/.bazuka-wallet && rm -rf ~/.bazuka
+echo "Hello from ziesha_upd.sh"
+printenv
+#cd bazuka && git pull && cargo update && cargo install --path . && rm -rf ~/.bazuka ~/.bazuka-wallet && rm -rf ~/.bazuka
 bazuka init --mnemonic "$MNEMONIC"
-sudo systemctl restart ziesha
-sleep 20
-$HOME/.cargo/bin/bazuka node status | grep -e "version" -e "height" -e "network"
-sleep 5
-$HOME/.cargo/bin/bazuka node status | grep "height"
-sleep 5
-$HOME/.cargo/bin/bazuka node status | grep "height"
-sleep 5
-$HOME/.cargo/bin/bazuka node status | grep "height"
+#sudo systemctl restart ziesha
+#sleep 20
+#$HOME/.cargo/bin/bazuka node status | grep -e "version" -e "height" -e "network"
+#sleep 5
+#$HOME/.cargo/bin/bazuka node status | grep "height"
+#sleep 5
+#$HOME/.cargo/bin/bazuka node status | grep "height"
+#sleep 5
+#$HOME/.cargo/bin/bazuka node status | grep "height"
