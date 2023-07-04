e702b0ca58ccde6075044cdac5eedb79d315106d
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2023-07-04 16:01:17 +0500

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -10,8 +10,10 @@
 #$HOME/.cargo/bin/bazuka init --mnemonic "$TEST"
 #echo "printenv from bash"
 #source ~/.bash_profile
-echo -c "   _____                     _                ____   ____   _   _ \n   |  _  \  __   _  _   __   | |   __    __   /   _\ /   _\ | |_| |"
-echo "  | | ) / / _\ | \/ | / _\ |   | / _\  |__|  \__  \ \__  \ |  _  |
-        |_| \_\ \__\ |_||_| \__/  |_|  \__\        \____/ \____/ |_| |_|"
+echo -c "
+ _____                     _                ____   ____   _   _ 
+|  _  \  __   _  _   __   | |   __    __   /   _\ /   _\ | |_| |
+| | ) / / _\ | \/ | / _\ |   | / _\  |__|  \__  \ \__  \ |  _  |
+|_| \_\ \__\ |_||_| \__/  |_|  \__\        \____/ \____/ |_| |_|"
 echo "printenv from bash" && printenv PATH TEST TEST2
 bazuka node status | grep -e "version" -e "height" -e "network"
