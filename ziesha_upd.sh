03f24a9e1bc791f648ef31c93d807c9efee319ee
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2023-07-05 16:41:58 +0500

    Update ziesha_upd.sh

--- ziesha_upd.sh
+++ ziesha_upd.sh
@@ -2,10 +2,10 @@ cd bazuka && git pull -q && cargo update && cargo install --path . && rm -rf ~/.
 bazuka init --mnemonic "$MNEMONIC"
 sudo systemctl restart ziesha
 sleep 20
-$HOME/.cargo/bin/bazuka node status | grep -e "version" -e "height" -e "network"
+bazuka node status | grep -e "version" -e "height" -e "network"
 sleep 5
-$HOME/.cargo/bin/bazuka node status | grep "height"
+bazuka node status | grep "height"
 sleep 5
-$HOME/.cargo/bin/bazuka node status | grep "height"
+bazuka node status | grep "height"
 sleep 5
-$HOME/.cargo/bin/bazuka node status | grep "height"
+bazuka node status | grep "height"
