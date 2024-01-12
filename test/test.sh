0da7cf0e919622a9a5ec537a4e748f1cfd7f070e
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-01-12 15:05:03 +0500

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -20,13 +20,13 @@ echo -e "
 
 echo "print PATH" && echo $PATH
 
-ver="1.21.6"
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
+echo "set ver" && ver="1.21.6"
+echo "download go" && wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+echo "rm old go ver" && sudo rm -rf /usr/local/go
+echo "unpack go" && sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+echo "del arch" && rm "go$ver.linux-amd64.tar.gz"
 #echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-echo "export PATH=\$PATH:/usr/local/go/bin" >>~/.profile
-echo "export PATH=\$PATH:\$(go env GOPATH)/bin" >>~/.profile
+echo "export PATH to .profile" && echo "export PATH=\$PATH:/usr/local/go/bin" >>~/.profile
+echo "export work PATH to .profile" && echo "export PATH=\$PATH:\$(go env GOPATH)/bin" >>~/.profile
 source ~/.profile
 go version
