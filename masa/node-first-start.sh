bf5bc434173a2e9381cd7ce6de1689b7aa33a45c
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-01-12 18:38:48 +0500

    Update node-first-start.sh

--- masa/node-first-start.sh
+++ masa/node-first-start.sh
@@ -1,3 +1,3 @@
 cd masa-oracle-go-testnet
-# Запуск ноды в интерактивном режиме на 10 сек.
-timeout 10s ./masa-node --start
+# Запуск ноды в интерактивном режиме на 10 сек. "|| true" - вернет 0, даже если процесс будет убит по таймауту.
+timeout 10s ./masa-node --start || true
