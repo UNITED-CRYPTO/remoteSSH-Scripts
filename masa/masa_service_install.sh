a6b121150312b728bb242a787af4dce4237aa0a9
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-01-19 13:07:22 +0500

    Create masa_service_install.sh

---
+++ masa/masa_service_install.sh
@@ -0,0 +1,24 @@
+#sudo tee /etc/systemd/system/masad.service > /dev/null <<EOF
+sudo tee /root/masad.service > /dev/null <<EOF
+
+[Unit]
+Description=Masa Node
+After=network.target
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=/root/masa-oracle-go-testnet/
+ExecStart=/root/masa-oracle-go-testnet/masa-node --bootnodes=/ip4/35.224.231.145/udp/4001/quic-v1/p2p/16Uiu2HAm47nBiewWLLzCREtY8vwPQtr5jTqyrEoUo6WnngwhsQuR,/ip4/104.198.43.138/udp/4001/quic-v1/p2p/16Uiu2HAkxiP8jjdHQWeCxTr7pD6BvoPkS8Z1skjCy9vdSRMACDcc,/ip4/35.202.227.74/udp/4001/quic-v1/p2p/16Uiu2HAmHuUejpUBFPCxy32QhGRAbv3tFwbzXmLkCoaNcZTyWWqN,/ip4/10.128.0.47/udp/4001/quic-v1/p2p/16Uiu2HAkxiP8jjdHQWeCxTr7pD6BvoPkS8Z1skjCy9vdSRMACDcc,/ip4/107.223.13.174/udp/4001/quic-v1/p2p/16Uiu2HAm2uQ5TGviRkqhYMpg7fjeoB4TfpSAhrbY87YZ4h9jYCNm,/ip4/34.171.201.124/udp/4001/quic-v1/p2p/16Uiu2HAmCKzfsynicpryPZTdcJsjmyzXn8tA13zMHHsoBxLdvVCE,/ip4/34.132.48.64/udp/4001/quic-v1/p2p/16Uiu2HAmNk4DDNiVu8ipN2cg5GLpGzN6ydd4EYps1NkiTDBRkctu --port=4001 --udp=true --start=true
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#sudo systemctl daemon-reload
+
+#sudo systemctl enable masad
+
+#sudo systemctl start masad
