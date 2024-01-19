f0d23399aeea54c1a91b1bc1ab4f47e332b93a01
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-01-19 14:22:32 +0500

    Update port_change.sh

--- masa/port_change.sh
+++ masa/port_change.sh
@@ -1,5 +1,7 @@
 #Нода Masa по умолчанию использует порт 8080. Но если порт на сервере используется, то сервис Masa выдаст ошибку "[ERROR] listen tcp :8080: bind: address already in use". Здесь мы меняем порт на другой, который свободен (см. sudo ss -tulpn)
 
+sudo ss -tulpn | grep 8080
+
 sudo tee /root/.masa/masa_oracle_node.env > /dev/null <<EOF
 private.key=/root/.masa/masa_oracle_key
 PORT=8081
