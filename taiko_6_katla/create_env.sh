cd33630392fbb9ebbe862583603f7e0db23306f2
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-03-01 19:02:13 +0500

    Update create_env.sh

--- taiko_6_katla/create_env.sh
+++ taiko_6_katla/create_env.sh
@@ -73,4 +73,4 @@ TXPOOL_LOCALS=
 # Timeout when waiting for a propose or prove block transaction receipt to be seen, in seconds
 WAIT_RECEIPT_TIMEOUT=360
 EOF
-cat $HOME/simple-taiko-node/.env
+#cat $HOME/simple-taiko-node/.env
