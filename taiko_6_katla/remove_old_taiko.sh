f28046b7fd48c1b07c3a5e3421fdaafa3d48bbb8
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-03-18 19:21:18 +0500

    Update remove_old_taiko.sh

--- taiko_6_katla/remove_old_taiko.sh
+++ taiko_6_katla/remove_old_taiko.sh
@@ -1,5 +1,10 @@
 #Удаление старых контейнеров
-cd simple-taiko-node/
-docker compose down -v
-cd ~
-mv simple-taiko-node/ simple-taiko-node_old/
+
+if [ -d $HOME/simple-taiko-node ] ; then 
+	cd $HOME/simple-taiko-node
+	docker compose down -v
+	cd $HOME
+	mv simple-taiko-node/ simple-taiko-node_old/
+else
+	echo "There is no $HOME/simple-taiko-node"
+fi
