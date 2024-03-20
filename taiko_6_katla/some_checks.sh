40c65006c295d00854fb871d15af3b0b833168f5
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-03-20 14:41:31 +0500

    Update some_checks.sh

--- taiko_6_katla/some_checks.sh
+++ taiko_6_katla/some_checks.sh
@@ -1,3 +1,3 @@
 dir
 df -hT | grep sda3 | awk '{print $1 echo '-' $5 echo "G Available"}'
-echo "Dashboard address=$(hostname -I | awk '{print $1}'):3001"
+echo "Dashboard address: http://$(hostname -I | awk '{print $1}'):3001"
