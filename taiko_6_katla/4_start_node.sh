d7fac3af42c19ba86d1fa7d6f570c3bc4f6ab7fa
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-03-20 14:08:10 +0500

    Update 4_start_node.sh

--- taiko_6_katla/4_start_node.sh
+++ taiko_6_katla/4_start_node.sh
@@ -1,12 +1,16 @@
 cd $HOME/simple-taiko-node
 if ! sudo ss -tulpn | grep LISTEN | awk '{print $5}' |  grep -e 8547 -e 8548 -e 6060 -e 30306 -e 9876 -e 9091 -e 3001; then 
 	echo "All needed ports are free" 
-	echo "Starting Taiko..." && docker compose up -d	
+	echo "Starting Taiko..." && docker compose up -d
+	timeout 20s docker compose logs -f || true
+	docker compose ps
 elif [ -d /opt/outline/ ] && docker ps | grep -q shadowbox ; then 
 	echo "Some needed ports are occupied. Outline is here - probably it's a reason"
 	echo "Stopping Outline..." && docker stop shadowbox watchtower
 	echo "Starting Taiko..." && docker compose up -d	
 	echo "Starting Outline..." && docker start shadowbox watchtower
+	timeout 20s docker compose logs -f || true
+	docker compose ps
 else
 	echo "Some needed ports are occupied and no Outline here. Plese check the reason"
 fi
