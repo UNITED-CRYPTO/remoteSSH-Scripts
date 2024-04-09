34680015a4e2c5cbadeb74e5b54bdb0b6f900f8c
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-04-09 16:32:28 +0500

    Update auto_vote.sh

--- namada/auto_vote.sh
+++ namada/auto_vote.sh
@@ -1,4 +1,24 @@
-VAR=611
+VAR=613
+echo $VAR
 ./auto_vote.sh $VAR
-VAR=612
+VAR=614
+echo $VAR
+./auto_vote.sh $VAR
+VAR=615
+echo $VAR
+./auto_vote.sh $VAR
+VAR=616
+echo $VAR
+./auto_vote.sh $VAR
+VAR=617
+echo $VAR
+./auto_vote.sh $VAR
+VAR=618
+echo $VAR
+./auto_vote.sh $VAR
+VAR=619
+echo $VAR
+./auto_vote.sh $VAR
+VAR=620
+echo $VAR
 ./auto_vote.sh $VAR
