54dfdb6228d15e6c293c30a6c8c92eb23cebb5c4
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-04-09 16:22:46 +0500

    Update auto_vote.sh

--- namada/auto_vote.sh
+++ namada/auto_vote.sh
@@ -1,11 +1,4 @@
-#Удаление старых контейнеров
-
-if [ -d $HOME/simple-taiko-node ] ; then 
-	echo "There is $HOME/simple-taiko-node, removing..."
-	cd $HOME/simple-taiko-node
-	docker compose down -v
-	cd $HOME
-	mv simple-taiko-node/ simple-taiko-node_old/
-else
-	echo "There is no $HOME/simple-taiko-node"
-fi
+VAR=610
+./auto_vote.sh $VAR
+VAR=611
+./auto_vote.sh $VAR
