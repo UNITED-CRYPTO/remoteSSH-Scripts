061938816533b951bcfe36aaec2ae7eb3889f145
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-04-10 12:04:24 +0500

    Update auto_unjail.sh

--- namada/auto_unjail.sh
+++ namada/auto_unjail.sh
@@ -1,11 +1,1 @@
-#Удаление старых контейнеров
-
-if [ -d $HOME/simple-taiko-node ] ; then 
-	echo "There is $HOME/simple-taiko-node, removing..."
-	cd $HOME/simple-taiko-node
-	docker compose down -v
-	cd $HOME
-	mv simple-taiko-node/ simple-taiko-node_old/
-else
-	echo "There is no $HOME/simple-taiko-node"
-fi
+ ./auto_unjail.sh
