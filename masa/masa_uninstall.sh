1ca9fedf1cf9688f4edcd3a32113aa1716f772ff
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-06-28 18:19:29 +0500

    Update masa_uninstall.sh

--- masa/masa_uninstall.sh
+++ masa/masa_uninstall.sh
@@ -1,2 +1,12 @@
-ls -la
+# Останов и удаление сервиса
+sudo systemctl stop masad
+sudo systemctl disable masad
+sudo systemctl daemon-reload
 
+# Удаление данных
+rm - rf masa-oracle-go-testnet
+sudo rm /etc/systemd/system/masad.service
+
+# Проверка
+sudo cat /etc/systemd/system/masad.service
+ls -la
