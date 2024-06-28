50bdfd8c0d031665ec0e7d92daaa10ab3af989e5
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-06-28 18:38:47 +0500

    Update 1_remove_old_taiko.sh

--- taiko_6_katla/1_remove_old_taiko.sh
+++ taiko_6_katla/1_remove_old_taiko.sh
@@ -1,4 +1,4 @@
-#Удаление старых контейнеров
+# Удаление старых контейнеров
 
 if [ -d $HOME/simple-taiko-node ] ; then 
 	echo "There is $HOME/simple-taiko-node, removing..."
@@ -9,4 +9,15 @@ if [ -d $HOME/simple-taiko-node ] ; then
  	echo "simple-taiko-node removed!"
 else
 	echo "There is no $HOME/simple-taiko-node"
+fi
+
+
+# Удаление старых директорий
+
+if [ -d $HOME/simple-taiko-node_old ] ; then 
+	echo "There is $HOME/simple-taiko-node_old, removing..."
+	rm -rf simple-taiko-node_old
+ 	echo "simple-taiko-node_old removed!"
+else
+	echo "There is no $HOME/simple-taiko-node_old"
 fi
