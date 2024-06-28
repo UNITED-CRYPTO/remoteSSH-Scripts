8d95747569bacc4b156dbd8c2bd2a859ab8d460f
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-06-28 18:33:35 +0500

    Update 1_remove_old_taiko.sh

--- taiko_6_katla/1_remove_old_taiko.sh
+++ taiko_6_katla/1_remove_old_taiko.sh
@@ -2,10 +2,11 @@
 
 if [ -d $HOME/simple-taiko-node ] ; then 
 	echo "There is $HOME/simple-taiko-node, removing..."
-	#cd $HOME/simple-taiko-node
-	#docker compose down -v
-	#cd $HOME
-	#mv simple-taiko-node/ simple-taiko-node_old/
+	cd $HOME/simple-taiko-node
+	docker compose down -v
+	cd $HOME
+	mv simple-taiko-node/ simple-taiko-node_old/
+ 	echo "simple-taiko-node removed!"
 else
 	echo "There is no $HOME/simple-taiko-node"
 fi
