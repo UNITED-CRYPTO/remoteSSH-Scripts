630d1278b6dd0deff9470c67cdb6ef6887e90b12
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-01 13:33:25 +0500

    Update meson_uninstall.sh

--- meson/meson_uninstall.sh
+++ meson/meson_uninstall.sh
@@ -1,35 +1,25 @@
-cd ./meson_cdn-linux-amd64			
-			
-sudo ./meson_cdn config set --token=[your token]--https_port=443 --cache.size=30               #config			
-sudo ./service start meson_cdn                # start node			
-sudo ./service status meson_cdn               # check node running status			
-			
-Other commands			
-sudo ./service stop meson_cdn                 # stop node			
-sudo ./service remove meson_cdn               # remove node			
-sudo ./service restart meson_cdn              # restart node			
-sudo ./meson_cdn log                          # check logs			
-./meson_cdn -h                                # check help			
-			
-			
-GaGaNode commands			
-curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64         #download			
-			
-cd ./apphub-linux-amd64			
-			
-sudo ./apphub service install         #Install			
-sudo ./apphub service start       #Start			
-./apphub status       #Status			
-sudo ./apps/gaganode/gaganode config set --token=gcbxmzcyisdamkjs2c15b95c4834394e         #Set token			
-./apphub restart         #Restart			
-			
-Other commands			
-sudo ./apphub service install                    # install node			
-sudo ./apphub service start                      # start node			
-sudo ./apphub service stop                       # stop node			
-sudo ./apphub service remove                     # remove node			
-./apphub status                                  # check node running status			
-./apphub restart                                 # restart node			
-./apphub upgrade                                 # upgrade node			
-./apphub log                                     # check logs			
-./apphub -h                                      # check help			
+# Meson commands
+if [ -d $HOME/meson_cdn-linux-amd64 ] ; then 
+	echo "There is $HOME/meson_cdn-linux-amd64, removing..."
+	cd $HOME/meson_cdn-linux-amd64
+	sudo ./service stop meson_cdn
+	sudo ./service remove meson_cdn
+	cd $HOME
+	mv meson_cdn-linux-amd64/ meson_cdn-linux-amd64_old/
+ 	echo "meson_cdn-linux-amd64 removed!"
+else
+	echo "There is no $HOME/meson_cdn-linux-amd64"
+fi
+
+# GaGaNode commands
+if [ -d $HOME/apphub-linux-amd64 ] ; then 
+	echo "There is $HOME/apphub-linux-amd64, removing..."
+	cd $HOME/apphub-linux-amd64
+	sudo ./apphub service stop
+	sudo ./apphub service remove
+	cd $HOME
+	mv apphub-linux-amd64/ apphub-linux-amd64_old/
+ 	echo "apphub-linux-amd64 removed!"
+else
+	echo "There is no $HOME/apphub-linux-amd64"
+fi
