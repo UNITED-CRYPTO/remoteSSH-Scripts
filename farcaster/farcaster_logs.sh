9a15d098ba1cbc4cfac27831ec5ebc826c20ba51
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-03 12:48:01 +0500

    Update farcaster_logs.sh

--- farcaster/farcaster_logs.sh
+++ farcaster/farcaster_logs.sh
@@ -1,3 +1,3 @@
 # Журналы
 cd /root/farcaster/apps/hubble/
-echo "check last 20 sec. logs" && timeout 20s docker compose logs -f hubble || true
+echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
