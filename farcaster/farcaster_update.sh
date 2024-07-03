023fc121b3374aedbb78d41b036e6399fea1cccd
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-03 17:14:16 +0500

    Update farcaster_update.sh

--- farcaster/farcaster_update.sh
+++ farcaster/farcaster_update.sh
@@ -1,13 +1,13 @@
 if [ -d $HOME/farcaster ] ; then
 
- echo "There is farcaster. Trying uninastall..."
+ echo "There is farcaster. Trying to update..."
  cd /root/farcaster/apps/hubble/
- echo "Stopping and remove containers..." && docker compose down -v
- cd ~
- echo "Deleting farcaster dir..." && rm -rf farcaster/ && echo "Farcaster uninstalled!"
+ echo "Making git checkout main && git pull..." && git checkout main && git pull
+ echo "Restarting docker containers..." && docker compose stop && docker compose up -d --force-recreate --pull always
+ echo "Check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
 
 else
 
- echo "There is no farcaster, nothong to do"
+ echo "There is no farcaster, nothong to do."
 
 fi
