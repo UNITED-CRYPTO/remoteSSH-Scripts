995b34a6caeafc5f3e700ab1d3ae9cf7bae82fdb
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-17 16:28:11 +0500

    Update farcaster_logs.sh

--- farcaster/farcaster_logs.sh
+++ farcaster/farcaster_logs.sh
@@ -1,3 +1,13 @@
 # Журналы
-cd /root/farcaster/apps/hubble/
-echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
+
+if [ -d $HOME/farcaster ] ; then
+
+ echo "There is farcaster. Printing logs..."
+ cd /root/farcaster/apps/hubble/
+ echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
+
+else
+
+ echo "There is no farcaster, nothong to do"
+
+fi
