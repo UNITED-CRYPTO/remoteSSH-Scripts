1ac752a70e860a520fed6af5b867029a4abd5361
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-24 19:05:36 +0500

    Update nubit_install.sh

--- nubit/nubit_install.sh
+++ nubit/nubit_install.sh
@@ -1,57 +1,68 @@
-if [ -d $HOME/farcaster ] ; then
+if [ -d $HOME/nubit-node ] ; then
 
- echo "There is already farcaster. No need to install."
- #cd /root/farcaster/apps/hubble/
- #echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
+ echo "There is already nubit. No need to install."
 
 else
 
- echo "There is no farcaster. Trying to install..."
+ echo "There is no nubit. Trying to install..."
+ mkdir nubit-node
+	cd nubit-node
 
- # Клонируем репо farcaster
- git clone --recurse-submodules  https://github.com/farcasterxyz/hub-monorepo.git farcaster
+ echo "Creating docker file..."
+ sudo tee Dockerfile > /dev/null <<EOF
+FROM ubuntu:latest
 
- cd /root/farcaster/apps/hubble/
+# Install needed utilities
+RUN apt-get update && apt-get install -y \
+    curl \
+    wget \
+    tar \
+    procps \
+    bash \
+    coreutils \
+    gawk \
+    sed \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
- # Создадим рабочие директории для контейнера и на всякий случай зададим права текущего пользователя на папки .hub .rocks
- sudo mkdir .hub/ .rocks/
- sudo chown -R $USER:$USER .hub .rocks
- sudo chmod -R 777 .hub .rocks
+# Set work DIR
+WORKDIR /root
 
- # Зададим права доступа для контейнера Grafana к директории /root/farcaster/apps/hubble/grafana/data
- sudo mkdir ./grafana/data
- sudo chown -R 472:472 ./grafana/data
-
- # Генерируем пару ключей идентификации с помощью Docker Compose
- docker compose run hubble yarn identity create
-
- # В норме должно выдать такое:
- #{"hostname":"[some_hostname]","pid":28,"time":1719918738294,"level":30,"msg":"Flushing rust log buffer. Writing logs directly from Rust","flush_next":true}
- #{"level":30,"time":1719918738295,"pid":28,"hostname":"[some_hostname]","msg":"Wrote peerId: [some ID] to #/home/node/app/apps/hubble/.hub/[some ID]_id.protobuf"}
- #{"level":30,"time":1719918738294,"pid":28,"hostname":"[some_hostname]","msg":"Wrote peerId: [some ID] to /home/node/app/apps/hubble/.hub/default_id.protobuf"}
-
- # Создание файла .env скриптом
-sudo tee .env > /dev/null <<EOF
-# Set this to your L1 Mainnet ETH RPC URL
-ETH_MAINNET_RPC_URL=$ETH_RPC
-
-# Set this to your L2 Optimism Mainnet RPC URL
-OPTIMISM_L2_RPC_URL=$OPTIMISM_RPC
-
-# Set this to your Farcaster FID
-HUB_OPERATOR_FID=$MY_FID
-
-FC_NETWORK_ID=1
-BOOTSTRAP_NODE=/dns/nemes.farcaster.xyz/tcp/2282
-
-STATSD_METRICS_SERVER=statsd:8125
+# Download and run script
+CMD curl -sL1 https://nubit.sh | bash && tail -f /var/log/nubit.log
 
 EOF
 
- # Запуск контейнера с нодой и мониторингом
- docker compose up hubble statsd grafana -d
- # Если видим Hubble requires at least 16GB of RAM to run. Detected 7GB, то сервер имеет недостаточно памяти
+ echo "Creating docker-compose.yml file..."
+ sudo tee docker-compose.yml > /dev/null <<EOF
+services:
+  nubit-node:
+    build: .
+    container_name: nubit-node
+    volumes:
+      - ~/nubit-node/nubit-data:/root/nubit-node
+      - ~/nubit-node/.nubit-light-nubit-alphatestnet-1:/root/.nubit-light-nubit-alphatestnet-1
+    ports:
+      - "26657:26657"
+      - "26658:26658"
+      - "26659:26659"
+      - "9191:9090"
+      - "2121:2121/udp"
+      - "2121:2121"
+    restart: always
 
+volumes:
+  nubit-data:
+    driver: local
+
+EOF
+
+ # Сборка образа и запуск контейнера
+ echo "Building docker image..."
+ docker-compose build
+
+ echo "Staring docker..."
+ 
  # Журналы
  echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
 
