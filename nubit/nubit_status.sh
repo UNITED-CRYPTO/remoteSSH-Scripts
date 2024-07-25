78fbf99d79d93b365ae3e30ab1ee98bc40776936
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-25 18:43:59 +0500

    Update nubit_status.sh

--- nubit/nubit_status.sh
+++ nubit/nubit_status.sh
@@ -1,83 +1,20 @@
 if [ -d $HOME/nubit-node ] ; then
 
- echo "There is already nubit. No need to install."
+ echo "There is nubit. Checking status..."
+ cd nubit-node
+ output=$(docker compose ps)
 
+ ### Проверяем, содержит ли вывод пустой заголовок docker compose ps
+ if [ "$(echo "$output" | wc -l)" -eq 1 ]; then
+  echo "ERROR: node's container is not working"
+ else
+  echo "Node's container is working!"
+  docker compose ps
+  echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 || true
+ fi
+  
 else
 
- echo "There is no nubit. Trying to install..."
- mkdir nubit-node
- cd nubit-node
-
- echo "Creating docker file..."
- sudo tee Dockerfile > /dev/null <<EOF
-FROM ubuntu:latest
-
-# Install needed utilities
-RUN apt-get update && apt-get install -y \
-    curl \
-    wget \
-    tar \
-    procps \
-    bash \
-    coreutils \
-    gawk \
-    sed \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-
-# Set work DIR
-WORKDIR /root
-
-# Download and run script
-CMD curl -sL1 https://nubit.sh | bash && tail -f /var/log/nubit.log
-
-EOF
-
- echo "Creating docker-compose.yml file..."
- sudo tee docker-compose.yml > /dev/null <<EOF
-services:
-  nubit-node:
-    build: .
-    container_name: nubit-node
-    volumes:
-      - ~/nubit-node/nubit-data:/root/nubit-node
-      - ~/nubit-node/.nubit-light-nubit-alphatestnet-1:/root/.nubit-light-nubit-alphatestnet-1
-    ports:
-      - "26657:26657"
-      - "26658:26658"
-      - "26659:26659"
-      - "9191:9090"
-      - "2121:2121/udp"
-      - "2121:2121"
-    restart: always
-
-volumes:
-  nubit-data:
-    driver: local
-
-EOF
-
- # Increase send net buffer size
- echo "Increase send net buffer size..."
- sudo sysctl -w net.core.rmem_max=2621440
- sudo sysctl -w net.core.rmem_default=2621440
- sudo sysctl -w net.core.wmem_max=2621440
- sudo sysctl -w net.core.wmem_default=2621440
- sudo sysctl -w net.core.optmem_max=2621440
- sudo sysctl -p
- sudo systemctl restart docker
- sleep 5
+ echo "ERROR: There is no nubit. Nothing to do"
  
- # Сборка образа и запуск контейнера
- echo "Building docker image..."
- docker compose build
- sleep 5
-
- echo "Staring docker..."
- docker compose up -d
- sleep 5
- 
- # Журналы
- echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 || true
-
 fi
