3b137d6ef25cd5c68f61d8d917dc574f63a8d6b9
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-25 19:25:37 +0500

    Update nubit_status.sh

--- nubit/nubit_status.sh
+++ nubit/nubit_status.sh
@@ -7,9 +7,12 @@ if [ -d $HOME/nubit-node ] ; then
  ### Проверяем, содержит ли вывод пустой заголовок docker compose ps
  if [ "$(echo "$output" | wc -l)" -eq 1 ]; then
   echo "ERROR: node's container is not working"
+  echo "check last logs" && timeout 20s docker compose logs -f --tail 100 || true
  else
   echo "Node's container is working!"
   docker compose ps
+  echo "Node status:"
+ 	docker exec -it nubit-node $HOME/nubit-node/bin/nubit das sampling-stats --node.store $HOME/.nubit-light-nubit-alphatestnet-1
   echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 || true
  fi
   
