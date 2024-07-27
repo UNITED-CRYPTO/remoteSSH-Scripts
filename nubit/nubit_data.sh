f12b71f41c13fe79eafe676fefcbf48ac2220aa2
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-07-27 22:10:23 +0500

    Update nubit_data.sh

--- nubit/nubit_data.sh
+++ nubit/nubit_data.sh
@@ -6,8 +6,10 @@ if [ -d $HOME/nubit-node ] ; then
  
  echo "Printing NAME / ADDRESS / PUBKEY..."
  docker exec -it nubit-node $HOME/nubit-node/bin/nkey list --p2p.network nubit-alphatestnet-1 --node.type light
- 
- echo "MNEMONIC: $(cat $HOME/nubit-node/nubit-data/mnemonic.txt)" 
+
+ if [ -e $HOME/nubit-node/nubit-data/mnemonic.txt ]; then
+  echo "MNEMONIC: $(cat $HOME/nubit-node/nubit-data/mnemonic.txt)"
+	fi
  
  echo "Printing BALANCE..."
  docker exec -it nubit-node $HOME/nubit-node/bin/nubit state balance --node.store $HOME/.nubit-light-nubit-alphatestnet-1
