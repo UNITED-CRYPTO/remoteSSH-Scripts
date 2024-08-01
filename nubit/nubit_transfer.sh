f7c591db5432d82cc6cc6a3941c44130639a3d9d
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-08-01 16:28:03 +0500

    Update nubit_transfer.sh

--- nubit/nubit_transfer.sh
+++ nubit/nubit_transfer.sh
@@ -12,23 +12,19 @@ if [ -d $HOME/nubit-node ] ; then
   echo "Node's container is working!"
   docker compose ps
 
-  # Генерация случайного числа задержки в диапазоне от 1 до 600
-  random_sleep=$(shuf -i 1-600 -n 1)
+  # Генерация случайного числа задержки в диапазоне от 1 до 6
+  random_sleep=$(shuf -i 1-6 -n 1)
 	
   # Генерация случайного числа количества в диапазоне от 1 до 100
   random_amount=$(shuf -i 1-100 -n 1)
 
   # Задержка на случайное количество секунд
   echo "Спим $random_sleep секунд..."
-#  sleep $random_sleep
+  sleep $random_sleep
 	
   # Переводим токены
-#  docker exec -it nubit-node $HOME/nubit-node/bin/nubit state transfer $ADDRESS $random_amount 500 100000  --node.store $HOME/.nubit-light-nubit-alphatestnet-1
-
-  echo $ADDRESS
-  echo "$ADDRESS"
-  docker exec -it nubit-node $HOME/nubit-node/bin/nubit state account-address  --node.store $HOME/.nubit-light-nubit-alphatestnet-1
-  echo "random_amount: $random_amount"
+  echo "Transfering random amount: $random_amount"
+  docker exec -it nubit-node $HOME/nubit-node/bin/nubit state transfer $ADDRESS $random_amount 500 100000  --node.store $HOME/.nubit-light-nubit-alphatestnet-1
 
  fi
   
