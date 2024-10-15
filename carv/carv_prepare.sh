13c81650c9cd4b60908dc45fd03819378ae438c5
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-10-15 16:02:02 +0500

    Update carv_prepare.sh

--- carv/carv_prepare.sh
+++ carv/carv_prepare.sh
@@ -26,17 +26,21 @@ fi
 echo "screen version check"
 screen -v
 
-echo "Download and install Go"
-cd $HOME
-ver="1.21.3"
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+#echo "Download and install Go"
+#cd $HOME
+#ver="1.21.3"
+#wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+#sudo rm -rf /usr/local/go
+#sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+#rm "go$ver.linux-amd64.tar.gz"
+#echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+#source $HOME/.bash_profile
 
 echo "go version check"
 go version
+
+git clone https://github.com/carv-protocol/verifier.git
+cd verifier
+make build
 
 echo "Comlete!"
