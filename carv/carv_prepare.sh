75f64ce702f9407641bfd501c7aa99174e4c4903
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-10-15 14:12:56 +0500

    Update carv_prepare.sh

--- carv/carv_prepare.sh
+++ carv/carv_prepare.sh
@@ -1,17 +1,27 @@
-#sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
-#if command -v make &> /dev/null
-#then
-#    echo "make уже установлен"
-#    make -v
-#else
-#    echo "make не найден. Устанавливаю..."
-#    sudo apt install make -y
-#    make -v
-#fi
+if command -v make &> /dev/null
+then
+    echo "make уже установлен"
+    make -v
+else
+    echo "make не найден. Устанавливаю..."
+    sudo apt install make -y
+    make -v
+fi
 
 echo "make version check"
 make -v
+
+if command -v screen &> /dev/null
+then
+    echo "screen уже установлен"
+    screen -v
+else
+    echo "screen не найден. Устанавливаю..."
+    sudo apt install screen -y
+    screen -v
+fi
 
 echo "screen version check"
 screen -v
