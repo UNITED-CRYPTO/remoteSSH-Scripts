2612ae165f257349063be428ac59ebf31953b6b3
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-10-15 13:47:23 +0500

    Update carv_prepare.sh

--- carv/carv_prepare.sh
+++ carv/carv_prepare.sh
@@ -1,58 +1,20 @@
-if [ -d $HOME/farcaster ] ; then
+sudo apt update && sudo apt upgrade -y
 
- echo "There is already farcaster. No need to install."
- #cd /root/farcaster/apps/hubble/
- #echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
-
+if command -v make &> /dev/null
+then
+    echo "make уже установлен"
+    make -v
 else
-
- echo "There is no farcaster. Trying to install..."
-
- # Клонируем репо farcaster
- git clone --recurse-submodules  https://github.com/farcasterxyz/hub-monorepo.git farcaster
-
- cd /root/farcaster/apps/hubble/
-
- # Создадим рабочие директории для контейнера и на всякий случай зададим права текущего пользователя на папки .hub .rocks
- sudo mkdir .hub/ .rocks/
- sudo chown -R $USER:$USER .hub .rocks
- sudo chmod -R 777 .hub .rocks
-
- # Зададим права доступа для контейнера Grafana к директории /root/farcaster/apps/hubble/grafana/data
- sudo mkdir ./grafana/data
- sudo chown -R 472:472 ./grafana/data
-
- # Генерируем пару ключей идентификации с помощью Docker Compose
- docker compose run hubble yarn identity create
-
- # В норме должно выдать такое:
- #{"hostname":"[some_hostname]","pid":28,"time":1719918738294,"level":30,"msg":"Flushing rust log buffer. Writing logs directly from Rust","flush_next":true}
- #{"level":30,"time":1719918738295,"pid":28,"hostname":"[some_hostname]","msg":"Wrote peerId: [some ID] to #/home/node/app/apps/hubble/.hub/[some ID]_id.protobuf"}
- #{"level":30,"time":1719918738294,"pid":28,"hostname":"[some_hostname]","msg":"Wrote peerId: [some ID] to /home/node/app/apps/hubble/.hub/default_id.protobuf"}
-
- # Создание файла .env скриптом
-sudo tee .env > /dev/null <<EOF
-# Set this to your L1 Mainnet ETH RPC URL
-ETH_MAINNET_RPC_URL=$ETH_RPC
-
-# Set this to your L2 Optimism Mainnet RPC URL
-OPTIMISM_L2_RPC_URL=$OPTIMISM_RPC
-
-# Set this to your Farcaster FID
-HUB_OPERATOR_FID=$MY_FID
-
-FC_NETWORK_ID=1
-BOOTSTRAP_NODE=/dns/nemes.farcaster.xyz/tcp/2282
-
-STATSD_METRICS_SERVER=statsd:8125
-
-EOF
-
- # Запуск контейнера с нодой и мониторингом
- docker compose up hubble statsd grafana -d
- # Если видим Hubble requires at least 16GB of RAM to run. Detected 7GB, то сервер имеет недостаточно памяти
-
- # Журналы
- echo "check last 20 sec. logs" && timeout 20s docker compose logs -f --tail 100 hubble || true
-
+    echo "make не найден. Устанавливаю..."
+    sudo apt install make -y
+    make -v
 fi
+
+echo "git check"
+git version
+
+echo "make version check"
+make -v
+
+echo "screen version check"
+screen -v
