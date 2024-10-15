1a91b8d467e80bb3c356e7d3101bf77864665abb
SergeSh <116004336+Serge-GHSHCH@users.noreply.github.com>
2024-10-15 18:49:29 +0500

    Update carv_prepare.sh

--- carv/carv_prepare.sh
+++ carv/carv_prepare.sh
@@ -83,13 +83,13 @@ fi
 if check_session && ! is_session_dead; then
     echo "Сессия $SESSION_NAME успешно создана и активна."
     echo "Запускаем ноду"
-    #screen -S carv -X stuff "/root/verifier/bin/verifier -private-key $PRIVATE_KEY -reward-address $REVARD_ADDRESS -commission-rate 1 > /root/verifier/log.txt 2>&1\n"
-    echo "PRIVATE_KEY=$PRIVATE_KEY"
-    echo "REVARD_ADDRESS=$REVARD_ADDRESS"
+    screen -S carv -X stuff "/root/verifier/bin/verifier -private-key $PRIVATE_KEY -reward-address $REVARD_ADDRESS -commission-rate 1 > /root/verifier/log.txt 2>&1\n"
+    #echo "PRIVATE_KEY=$PRIVATE_KEY"
+    #echo "REVARD_ADDRESS=$REVARD_ADDRESS"
     echo "Попытка запустить ноду сделана, спим 60 сек..."
-    #sleep 60
+    sleep 60
     echo "Выводим лог ноды"
-    #tail -n100 /root/verifier/log.txt
+    tail -n100 /root/verifier/log.txt
 else
     echo "Ошибка: Сессия $SESSION_NAME не активна."
 fi
